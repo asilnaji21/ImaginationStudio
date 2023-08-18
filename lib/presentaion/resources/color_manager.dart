1dcffda0c5b378be759cc9a88ccd60365c458133
hh7113404@gmail.com <asilahmednaji@gmail.com>
2023-08-18 20:15:21 +0300

    I added Color Manager Modified

--- lib/presentaion/resources/color_manager.dart
+++ lib/presentaion/resources/color_manager.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class ColorManager {
   static const Color primaryMainEnableColor = Color(0xffC68570);
   static const Color primaryMainColor = Color(0xff9C9E8A);
+  static const Color borderColor = Color(0xff707070);
+  static const Color blackColor = Color(0xff050505);
+  static const Color textColor = Color(0xffEBE8E5);
 }
