e482e19b1f8437b5e53eeb641b70f6b2881521cb
hh7113404@gmail.com <asilahmednaji@gmail.com>
2023-08-18 20:16:28 +0300

    Done Welcome View

--- lib/presentaion/views/welcome/welcome_view.dart
+++ lib/presentaion/views/welcome/welcome_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../app/routes/navigator.dart';
+import '../../../app/routes/route_constants.dart';
 import '../../resources/color_manager.dart';
 import '../../resources/image_assets.dart';
+import '../../widgets/Base_button.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({Key? key}) : super(key: key);
@@ -19,30 +22,44 @@ class WelcomeView extends StatelessWidget {
           ),
           Align(
             alignment: Alignment.centerRight,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text("SignUp"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: ColorManager.primaryMainColor,
-                textStyle: TextStyle(fontSize: 22, color: Colors.white),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 424,
-            right: 11,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Login"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: ColorManager.primaryMainEnableColor,
-                  textStyle: TextStyle(fontSize: 22, color: Colors.white),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                BaseButton(
+                  buttonText: "Sign up",
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(RouteConstants.signUpRoute);
+                  },
                 ),
-              ),
+                const SizedBox(height: 2),
+                BaseButton(
+                  buttonText: "Sign in",
+                  buttonColor: ColorManager.primaryMainEnableColor,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(RouteConstants.signInRoute);
+                  },
+                )
+              ],
             ),
-          ),
+            // Positioned(
+            //   top: 424,
+            //   right: 11,
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(horizontal: 10),
+            //     child: ElevatedButton(
+            //       onPressed: () {
+            //         Navigator.of(context).pushNamed(RouteConstants.signInRoute);
+            //       },
+            //       child: Text("Login"),
+            //       style: ElevatedButton.styleFrom(
+            //         backgroundColor: ColorManager.primaryMainEnableColor,
+            //         textStyle: TextStyle(fontSize: 22, color: Colors.white),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+          )
         ],
       ),
     );
