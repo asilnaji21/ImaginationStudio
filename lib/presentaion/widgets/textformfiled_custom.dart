1799df3479444aa876793e53a850c719a91596a6
hadiaHani <hh7113404@gmail.com>
2023-08-24 22:31:40 +0300

    edit CustomTextFormField

--- lib/presentaion/widgets/textformfiled_custom.dart
+++ lib/presentaion/widgets/textformfiled_custom.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 
 import '../resources/color_manager.dart';
 
-// ignore: must_be_immutable
 class CustomTextFormField extends StatelessWidget {
   String? nameText;
   IconData? icon;
-
+  Color? iconColor;
+  bool? readOnly;
   TextInputType? keyboardType;
+  TextEditingController? controller;
+  void Function()? onPressed;
   CustomTextFormField({
     required this.nameText,
-    required this.icon,
+    this.icon,
+    this.controller,
+    this.readOnly,
+    this.iconColor,
     this.keyboardType,
+    this.onPressed,
     super.key,
   });
 
@@ -24,12 +30,20 @@ class CustomTextFormField extends StatelessWidget {
         width: 1,
       )),
       child: TextFormField(
+        controller: controller,
+        readOnly: readOnly ?? false,
         keyboardType: keyboardType ?? TextInputType.text,
         style: const TextStyle(fontFamily: "Arial"),
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.only(left: 16, top: 14),
           border: InputBorder.none,
-          suffixIcon: Icon(icon),
+          suffixIcon: IconButton(
+            icon: Icon(
+              icon,
+              color: iconColor,
+            ),
+            onPressed: onPressed,
+          ),
           hintText: nameText,
           hintStyle: const TextStyle(fontSize: 20, fontFamily: "Aldhabi"),
         ),
