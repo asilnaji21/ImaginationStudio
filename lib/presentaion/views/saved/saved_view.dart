01e316fd2594cbeb117a83f94b642cbaa63f8e9e
hadiaHani <hh7113404@gmail.com>
2023-08-28 19:19:36 +0300

    display favorite items

--- lib/presentaion/views/saved/saved_view.dart
+++ lib/presentaion/views/saved/saved_view.dart
@@ -1,75 +1,42 @@
 import 'package:flutter/material.dart';
 import '../../widgets/CustomSavedPage.dart';
+
 class SavedView extends StatelessWidget {
   const SavedView({super.key});
   @override
   Widget build(BuildContext context) {
+    var favorite = Provider.of<AppProvider>(context).favoriteList;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 43,
-            ),
-            const Text(
-              "Saved",
-              style: TextStyle(fontSize: 88),
-            ),
-            CustomSavedPage(
-              imageUrl: 'assets/Gardens.jpg',
-              namecategory: 'Mirror',
-              price: '80\$',
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            CustomSavedPage(
-              imageUrl: 'assets/Gardens.jpg',
-              namecategory: 'Mirror',
-              price: '80\$',
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            CustomSavedPage(
-              imageUrl: 'assets/Gardens.jpg',
-              namecategory: 'Mirror',
-              price: '80\$',
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            CustomSavedPage(
-              imageUrl: 'assets/Gardens.jpg',
-              namecategory: 'Mirror',
-              price: '80\$',
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            CustomSavedPage(
-              imageUrl: 'assets/Gardens.jpg',
-              namecategory: 'Mirror',
-              price: '80\$',
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            CustomSavedPage(
-              imageUrl: 'assets/Gardens.jpg',
-              namecategory: 'Mirror',
-              price: '80\$',
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            CustomSavedPage(
-              imageUrl: 'assets/Gardens.jpg',
-              namecategory: 'Mirror',
-              price: '80\$',
-            ),
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Saved",
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.w500, fontSize: 35),
         ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: favorite.isEmpty
+            ? const Center(
+                child: Text(
+                  "No Item Found",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                ),
+              )
+            : ListView.separated(
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    height: 10,
+                  );
+                },
+                itemCount: favorite.length,
+                itemBuilder: (context, index) {
+                  final product = favorite[index];
+                  return CustomSavedPage(
+                    productModel: product,
+                  );
+                }),
       ),
     );
   }
