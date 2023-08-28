7d76b9796e99376ad7c345d45099bab66dee6d5f
hadiaHani <hh7113404@gmail.com>
2023-08-28 19:21:57 +0300

    ServiceProviders

--- lib/presentaion/views/serviceprovider/contact_service_provider.dart
+++ lib/presentaion/views/serviceprovider/contact_service_provider.dart
@@ -5,41 +5,79 @@ import '../../widgets/CustomContactButton.dart';
 import '../../widgets/CustomImageStackContactServiceProvider.dart';
 import '../../widgets/search_boxCustom.dart';
 
-class ServiceProvider extends StatelessWidget {
+class ServiceProvider extends StatefulWidget {
   const ServiceProvider({super.key});
+
+  @override
+  State<ServiceProvider> createState() => _ServiceProviderState();
+}
+
+class _ServiceProviderState extends State<ServiceProvider> {
+  late List<ServiceProviderModel>? servicesProviderList;
+  @override
+  void initState() {
+    super.initState();
+    getServicesProvider();
+  }
+
+  TextEditingController searchTextController = TextEditingController();
+
+  List<ServiceProviderModel> getServicesProvider() {
+    servicesProviderList = Constant.serviceProvider
+        .where((element) => element.name
+            .toLowerCase()
+            .contains(searchTextController.text.toLowerCase()))
+        .toList();
+    return servicesProviderList!;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        const SizedBox(
-          height: 60,
-        ),
-        SearchBox(),
-        const SizedBox(
-          height: 60,
-        ),
-        CustomImageStackContactServiceProvider(
-          imagePath: 'assets/project.jpg',
-          locatioProvider: '"\nEl-Remal St.,Gaza,Palestine"',
-          nameProvider: 'Sara Ali',
-          imagePathPerson: 'assets/woman.png',
-        ),
-        CustomContactButton(),
-        const SizedBox(
-          height: 33,
-        ),
-        CustomImageStackContactServiceProvider(
-          imagePath: 'assets/project2.jpg',
-          locatioProvider: '\nAl-Nasser St., Gaza,Palestine',
-          nameProvider: 'Kayan Ali',
-          imagePathPerson: 'assets/man.png',
-        ),
-        CustomContactButton(),
-        const SizedBox(
-          height: 33,
-        ),
-      ]),
+      body: Column(
+        children: [
+          SearchBox(
+            controller: searchTextController,
+            onSubmitted: (p0) {
+              setState(() {
+                getServicesProvider();
+              });
+            },
+            onChanged: (p0) {
+              setState(() {
+                getServicesProvider();
+              });
+            },
+          ),
+          Expanded(
+            child: servicesProviderList != null &&
+                    servicesProviderList!.isNotEmpty
+                ? Padding(
+                    padding: const EdgeInsets.only(
+                      top: 12.0,
+                    ),
+                    child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        return CustomImageStackContactServiceProvider(
+                          model: servicesProviderList![index],
+                        );
+                      },
+                      separatorBuilder: (context, index) => const SizedBox(
+                        height: 10,
+                      ),
+                      itemCount: servicesProviderList!.length,
+                    ),
+                  )
+                : const Center(
+                    child: Text(
+                      "No Designer Found",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                  ),
+          ),
+        ],
+      ),
     );
   }
 }
