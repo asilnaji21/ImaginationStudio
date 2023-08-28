e865ec78c53a1f66120a5e4e58da315522f2bd0f
hadiaHani <hh7113404@gmail.com>
2023-08-28 19:22:57 +0300

    ServiceProviderModel

---
+++ lib/presentaion/views/serviceprovider/domain/model/service_provider_model.dart
@@ -0,0 +1,36 @@
+class ServiceProviderModel {
+  final String image;
+  final String name;
+  final String address;
+  final String about;
+  final double rating;
+  final String website;
+  final String phone;
+  final List<Project> projects;
+
+  ServiceProviderModel(
+      {required this.image,
+      required this.name,
+      required this.address,
+      required this.about,
+      required this.rating,
+      required this.website,
+      required this.phone,
+      required this.projects});
+}
+
+class Project {
+  final String projectName;
+  final String image;
+  final String name;
+  final String projectDetails;
+  final List<String> projectImages;
+
+  Project({
+    required this.projectName,
+    required this.image,
+    required this.name,
+    required this.projectDetails,
+    required this.projectImages,
+  });
+}
