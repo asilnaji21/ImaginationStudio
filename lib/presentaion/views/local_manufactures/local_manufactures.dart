7f1371bbe9334b8435ea06d041f796031d808a19
hadiaHani <hh7113404@gmail.com>
2023-08-30 18:35:08 +0300

    complete LocalManufactures view

--- lib/presentaion/views/local_manufactures/local_manufactures.dart
+++ lib/presentaion/views/local_manufactures/local_manufactures.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../resources/color_manager.dart';
 import '../../widgets/elevated_button_custom.dart';
@@ -15,6 +16,10 @@ class LocalManufacturesRoute extends StatefulWidget {
 }
 
 class _LocalManufacturesRoute extends State<LocalManufacturesRoute> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController lengthController = TextEditingController();
+  TextEditingController widthController = TextEditingController();
   Color selectedColor = Colors.transparent;
   TextEditingController textEditingController = TextEditingController();
   void changeColor(Color color) {
@@ -57,21 +62,52 @@ class _LocalManufacturesRoute extends State<LocalManufacturesRoute> {
     );
   }
 
+  String? imagePath = "";
+  String? imagePath2 = "";
+
+  List<String> projectPhotoURLs = [];
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      imagePath = pickedFile.path;
+
+      setState(() {
+        if (pickedFile != null) {
+          imagePath = pickedFile.path;
+        } else {
+          debugPrint('No image selected.');
+        }
+        projectPhotoURLs.add(imagePath!);
+      });
+    } else {}
+  }
+
+  Future<void> _pickGalleryImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      imagePath2 = pickedFile.path;
+
+      setState(() {
+        if (pickedFile != null) {
+          imagePath2 = pickedFile.path;
+        } else {
+          debugPrint('No image selected.');
+        }
+        projectPhotoURLs.add(imagePath2!);
+      });
+    } else {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.shopping_cart,
-              color: ColorManager.borderColor,
-            ),
-          )
-        ],
-      ),
+      appBar: AppBar(),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 50.0),
@@ -86,7 +122,8 @@ class _LocalManufacturesRoute extends State<LocalManufacturesRoute> {
                     fontWeight: FontWeight.w400),
               ),
               const CustomText(text: "Name"),
-              const CustomTextFormField(
+              CustomTextFormField(
+                controller: nameController,
                 icon: Icons.person_outline,
                 nameText: "Enter name",
               ),
@@ -94,7 +131,8 @@ class _LocalManufacturesRoute extends State<LocalManufacturesRoute> {
                 height: 18,
               ),
               const CustomText(text: "Phone"),
-              const CustomTextFormField(
+              CustomTextFormField(
+                controller: phoneController,
                 keyboardType: TextInputType.phone,
                 icon: Icons.phone,
                 nameText: "+972-1234-56789",
@@ -116,25 +154,27 @@ class _LocalManufacturesRoute extends State<LocalManufacturesRoute> {
                   CustomText(text: "Width"),
                 ],
               ),
-              const Row(
+              Row(
                 children: [
                   Expanded(
                     child: CustomTextFormField(
+                      controller: lengthController,
                       keyboardType: TextInputType.number,
                       nameText: "Enter length",
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 10,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 10,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 10,
                   ),
                   Expanded(
                     child: CustomTextFormField(
+                      controller: widthController,
                       keyboardType: TextInputType.number,
                       nameText: "Enter width",
                     ),
@@ -157,17 +197,61 @@ class _LocalManufacturesRoute extends State<LocalManufacturesRoute> {
                 height: 18,
               ),
               const CustomText(text: "Upload image"),
-              const CustomTextFormField(
-                icon: Icons.drive_folder_upload,
-                nameText: "",
+              OutlinedButton(
+                onPressed: _pickImage,
+                style: OutlinedButton.styleFrom(
+                  maximumSize: const Size(300, 55),
+                  padding: const EdgeInsets.only(left: 10, top: 5, right: 10),
+                  side: const BorderSide(
+                      color: ColorManager.borderColor), // تخصيص لون الحدود
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "        ",
+                      style: TextStyle(
+                          fontSize: 29,
+                          fontFamily: "Aldhabi",
+                          color: ColorManager.blackColor,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Icon(
+                      Icons.drive_folder_upload,
+                      color: ColorManager.textColor2,
+                    )
+                  ],
+                ),
               ),
               const SizedBox(
                 height: 18,
               ),
               const CustomText(text: "Upload gallery"),
-              const CustomTextFormField(
-                icon: Icons.drive_folder_upload,
-                nameText: "",
+              OutlinedButton(
+                onPressed: _pickGalleryImage,
+                style: OutlinedButton.styleFrom(
+                  maximumSize: const Size(300, 55),
+                  padding: const EdgeInsets.only(left: 10, top: 5, right: 10),
+                  side: const BorderSide(
+                      color: ColorManager.borderColor), // تخصيص لون الحدود
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "        ",
+                      style: TextStyle(
+                          fontSize: 29,
+                          fontFamily: "Aldhabi",
+                          color: ColorManager.blackColor,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Icon(
+                      Icons.drive_folder_upload,
+                      color: ColorManager.textColor2,
+                    )
+                  ],
+                ),
               ),
               const SizedBox(
                 height: 30,
@@ -181,7 +265,49 @@ class _LocalManufacturesRoute extends State<LocalManufacturesRoute> {
                     child: CustomElevatedButton(
                         height: 8,
                         width: 90,
-                        onPressed: () {},
+                        onPressed: () {
+                          if (nameController.text.isEmpty ||
+                              phoneController.text.isEmpty ||
+                              lengthController.text.isEmpty ||
+                              widthController.text.isEmpty ||
+                              textEditingController.text.isEmpty ||
+                              imagePath == "" ||
+                              imagePath2 == "" ||
+                              projectPhotoURLs.isEmpty) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text(
+                                    "Error",
+                                    style: TextStyle(
+                                        color: Colors.red, fontSize: 25),
+                                  ),
+                                  content: const Text(
+                                    "Please fill in all input fields.",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 28),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text(
+                                        "OK",
+                                        style: TextStyle(fontSize: 25),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            setState(() {});
+                            Navigator.pop(context);
+                          }
+                        },
                         color: ColorManager.primaryMainColor,
                         text: "OK",
                         fontSize: 20,
