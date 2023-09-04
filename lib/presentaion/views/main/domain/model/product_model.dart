5b421fc1e51948ef233961f75a40ca271a256414
hadiaHani <hh7113404@gmail.com>
2023-09-04 17:03:54 +0300

    add quantity/priceTotal

--- lib/presentaion/views/main/domain/model/product_model.dart
+++ lib/presentaion/views/main/domain/model/product_model.dart
@@ -6,14 +6,16 @@ class ProductModel {
   final String productDescription;
   final String productImage;
   final String productQuantity;
-
-  ProductModel({
-    required this.productId,
-    required this.productTitle,
-    required this.productPrice,
-    required this.productCategory,
-    required this.productDescription,
-    required this.productImage,
-    required this.productQuantity,
-  });
+  int quantity;
+  double priceTotal;
+  ProductModel(
+      {required this.productId,
+      required this.productTitle,
+      required this.productPrice,
+      required this.productCategory,
+      required this.productDescription,
+      required this.productImage,
+      required this.productQuantity,
+      this.quantity = 1,
+      this.priceTotal = 0.0});
 }
