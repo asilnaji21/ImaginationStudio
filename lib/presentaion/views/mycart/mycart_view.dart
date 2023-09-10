555a4e27b24cf77d0ad9eafecdd9d58ddc23c2c7
hadiaHani <hh7113404@gmail.com>
2023-09-10 16:32:09 +0300

    edit SelectPayment

--- lib/presentaion/views/mycart/mycart_view.dart
+++ lib/presentaion/views/mycart/mycart_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:project_app/app_provider.dart';
+import 'package:project_app/presentaion/views/main/main_view.dart';
 import 'package:provider/provider.dart';
 import '../../resources/color_manager.dart';
 import '../../widgets/custom_container_card.dart';
-import '../../widgets/custom_select_payment.dart';
 import '../../widgets/elevated_button_custom.dart';
 import '../../widgets/custom_cart_page.dart';
 import '../../widgets/custom_divider.dart';
@@ -18,117 +18,165 @@ class MyCart extends StatefulWidget {
 class _MyCartState extends State<MyCart> {
   String selectedPaymentMethod = 'Cash';
   bool isPaymentComplete = false;
-
-  void _showBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 33),
-          child: ListView(
-            children: [
-              const Text(
-                "card payment",
-                style: TextStyle(fontSize: 50),
-              ),
-              Image.asset('assets/card.jpg'),
-              const SizedBox(
-                height: 50,
-              ),
-              const CustomContainerCard(
-                hintText: "holder",
-              ),
-              const SizedBox(
-                height: 44,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 76,
-                      width: 157,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(
-                          width: 1,
-                          color: ColorManager.borderColor,
-                        ),
-                      ),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.all(16),
-                          hintText: "CVV",
-                          hintStyle: TextStyle(
-                              fontSize: 30, color: ColorManager.textColor2),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  Expanded(
-                    child: Container(
-                      height: 76,
-                      width: 157,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(
-                          width: 1,
-                          color: ColorManager.borderColor,
-                        ),
-                      ),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.all(16),
-                          hintText: "Expire",
-                          hintStyle: TextStyle(
-                              fontSize: 30, color: ColorManager.textColor2),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const CustomContainerCard(
-                hintText: "Card Number",
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              const CustomDividerWithPadding(),
-              const SizedBox(
-                height: 22,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 22),
-                child: CustomElevatedButton(
-                  onPressed: () {},
-                  width: 100,
-                  text: "Check",
-                  color: ColorManager.primaryMainColor,
-                  colortext: ColorManager.textColor,
-                ),
-              ),
-              const SizedBox(
-                height: 22,
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
+  TextEditingController holderController = TextEditingController();
+  TextEditingController cvvController = TextEditingController();
+  TextEditingController expirehController = TextEditingController();
+  TextEditingController cardNumberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     var cartList = Provider.of<AppProvider>(context).cartList;
+    void _showBottomSheet(BuildContext context) {
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 33),
+            child: ListView(
+              children: [
+                const Text(
+                  "card payment",
+                  style: TextStyle(fontSize: 50),
+                ),
+                Image.asset('assets/card.jpg'),
+                const SizedBox(
+                  height: 50,
+                ),
+                CustomContainerCard(
+                  controller: holderController,
+                  hintText: "holder",
+                ),
+                const SizedBox(
+                  height: 44,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 76,
+                        width: 157,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                            width: 1,
+                            color: ColorManager.borderColor,
+                          ),
+                        ),
+                        child: TextFormField(
+                          controller: cvvController,
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.all(16),
+                            hintText: "CVV",
+                            hintStyle: TextStyle(
+                                fontSize: 30, color: ColorManager.textColor2),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 76,
+                        width: 157,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                            width: 1,
+                            color: ColorManager.borderColor,
+                          ),
+                        ),
+                        child: TextFormField(
+                          controller: expirehController,
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.all(16),
+                            hintText: "Expire",
+                            hintStyle: TextStyle(
+                                fontSize: 30, color: ColorManager.textColor2),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomContainerCard(
+                  controller: cardNumberController,
+                  hintText: "Card Number",
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const CustomDividerWithPadding(),
+                const SizedBox(
+                  height: 22,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 22),
+                  child: CustomElevatedButton(
+                    onPressed: () {
+                      if (cvvController.text.isEmpty ||
+                          holderController.text.isEmpty ||
+                          expirehController.text.isEmpty ||
+                          cardNumberController.text.isEmpty) {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text(
+                                "Error",
+                                style:
+                                    TextStyle(color: Colors.red, fontSize: 25),
+                              ),
+                              content: const Text(
+                                "Please fill in all input fields.",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400, fontSize: 28),
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text(
+                                    "OK",
+                                    style: TextStyle(fontSize: 25),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        setState(() {});
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MainView()));
+                        cartList.length = 0;
+                      }
+                    },
+                    width: 100,
+                    text: "Check",
+                    color: ColorManager.primaryMainColor,
+                    colortext: ColorManager.textColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 22,
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -239,12 +287,52 @@ class _MyCartState extends State<MyCart> {
                     textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 50),
                   ),
-                  const CustomSelectPayment(),
+                  Column(
+                    children: <Widget>[
+                      RadioListTile(
+                        title: const Text(
+                          'Cash ',
+                          style: TextStyle(fontSize: 40, color: Colors.black),
+                        ),
+                        value: 'Cash',
+                        groupValue: selectedPaymentMethod,
+                        onChanged: (String? value) {
+                          setState(() {
+                            selectedPaymentMethod = value!;
+                          });
+                        },
+                      ),
+                      RadioListTile(
+                        title: const Text(
+                          'Card ',
+                          style: TextStyle(fontSize: 40, color: Colors.black),
+                        ),
+                        value: 'Card',
+                        groupValue: selectedPaymentMethod,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedPaymentMethod = value!;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  //    const CustomSelectPayment(),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 104),
                     child: CustomElevatedButton(
                       onPressed: () {
-                        _showBottomSheet(context);
+                        if (selectedPaymentMethod == 'Cash') {
+                          // انتقل إلى الشاشة الرئيسية هنا إذا كان الاختيار هو "Cash"
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const MainView()));
+                          cartList.length = 0;
+                        } else {
+                          // إظهار الـ _showBottomSheet هنا إذا كان الاختيار غير "Cash"
+                          _showBottomSheet(context);
+                        }
                       },
                       width: 300,
                       text: "Check Out",
